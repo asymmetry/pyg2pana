27259450851598aea4ddceb85060bcecd3490525
Chao Gu <guchao.pku@gmail.com>
2018-08-01 18:29:04 -0400

    Add g2p simulation result reader

--- pyg2pana/__init__.py
+++ pyg2pana/__init__.py
@@ -4,8 +4,9 @@ __version__ = '1.0.0'
 
 from .data import Data
 from .run_db import RunDB
+from .sim_result import SimResult
 
 from . import configs
 
-__all__ = ['Data', 'RunDB']  # classes
+__all__ = ['Data', 'RunDB', 'SimResult']  # classes
 __all__ += ['configs']  # modules
